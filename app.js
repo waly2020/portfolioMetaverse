da0bc34e306974299db01f54ac85dcd9f6b92110
waly2020 <walyguema@gmail.com>
2022-08-23 18:57:34 +0100

    teste

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+let back_skill = document.querySelectorAll(".back_skill");
+let paras = document.querySelectorAll(".para_skill");
+
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
@@ -11,3 +14,15 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+// back_skill.forEach(back => {
+
+//     console.log((back));
+//     // paras[back_skill.indexOf(back)].textContent = `${back.getAttribute('data-height')}%`;
+// });
+
+for (let i = 0; i < back_skill.length; i++) {
+    let back = back_skill[i];
+    back.style.height = `${back.getAttribute('data-height')}%`;
+    paras[i].textContent = `${back.getAttribute('data-height')}%`;
+}
